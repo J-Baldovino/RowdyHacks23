a0e99bb5e2f0e12adbaeffbf07d458b88b56b80a
Hans Landgrebe <hlxbl20@gmail.com>
2023-03-25 14:26:07 -0500

    Update game.py
    Allows you to turn

--- game.py
+++ game.py
@@ -9,16 +9,22 @@ running = True
 #Snake Head Position
 posX = 0
 posY = 0
-orientation = 0 #0 = North, 1 = East, 2 = South, 4 = West
+orientation = 0 #0 = North, 1 = East, 2 = South, 3 = West
 
 #Main Function
 while running:
-    orientation %= 4
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            orientation += 1
+            if event.key == pygame.K_UP  and orientation != 2:
+                orientation = 0
+            elif event.key == pygame.K_RIGHT and orientation != 3:
+                orientation = 1
+            elif event.key == pygame.K_DOWN and orientation != 0:
+                orientation = 2
+            elif event.key == pygame.K_LEFT and orientation != 1:
+                orientation = 3
     match orientation:
         case 0:
             posY += 1
