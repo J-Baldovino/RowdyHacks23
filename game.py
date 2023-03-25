bb03ca6c6d3c9fba0774dc57024f05b1475ebd72
Hans Landgrebe <hlxbl20@gmail.com>
2023-03-25 14:06:07 -0500

    Basic movement language
    Spins in place

--- game.py
+++ game.py
@@ -1,16 +1,37 @@
 import pygame
 
-#set up
+#Pygame set up
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+#Snake Head Position
+posX = 0
+posY = 0
+orientation = 0 #0 = North, 1 = East, 2 = South, 4 = West
+
+#Main Function
 while running:
+    orientation %= 4
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill("purple")
+        elif event.type == pygame.KEYDOWN:
+            orientation += 1
+    match orientation:
+        case 0:
+            posY += 1
+        case 1:
+            posX += 1
+        case 2:
+            posY -= 1
+        case 3:
+            posX -= 1
+        case default:
+            posY += 1
+    print("Orientation: " + str(orientation) + " PostionX: " + str(posX) + " PositionY: " + str(posY))
+    screen.fill("Purple")
 
     #render game
 
