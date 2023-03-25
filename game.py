b98f3f9e73fe929a3f6538d62dba360234a759d9
Hans Landgrebe <hlxbl20@gmail.com>
2023-03-25 15:15:43 -0500

    Update game.py
    Moved things into functions

--- game.py
+++ game.py
@@ -11,20 +11,18 @@ posX = 0
 posY = 0
 orientation = 0 #0 = North, 1 = East, 2 = South, 3 = West
 
-#Main Function
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP  and orientation != 2:
-                orientation = 0
-            elif event.key == pygame.K_RIGHT and orientation != 3:
-                orientation = 1
-            elif event.key == pygame.K_DOWN and orientation != 0:
-                orientation = 2
-            elif event.key == pygame.K_LEFT and orientation != 1:
-                orientation = 3
+def turnController(orientation, keys):
+    if keys[pygame.K_UP]  and orientation != 2:
+        orientation = 0
+    elif keys[pygame.K_RIGHT] and orientation != 3:
+        orientation = 1
+    elif keys[pygame.K_DOWN] and orientation != 0:
+        orientation = 2
+    elif keys[pygame.K_LEFT] and orientation != 1:
+        orientation = 3
+    return orientation
+
+def movement(orientation, posX, posY):
     match orientation:
         case 0:
             posY += 1
@@ -36,6 +34,18 @@ while running:
             posX -= 1
         case default:
             posY += 1
+    return (posX, posY)
+
+#Main Function
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            orientation = turnController(orientation, pygame.key.get_pressed())
+    position = movement(orientation, posX, posY)
+    posX = position[0]
+    posY = position[1]
     print("Orientation: " + str(orientation) + " PostionX: " + str(posX) + " PositionY: " + str(posY))
     screen.fill("Purple")
 
